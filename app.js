c5a58062cf2423b08414aa3e4bc488ed85b77ca8
chiharakenta <chiharakenta0519@gmail.com>
2020-03-19 16:03:53 +0900

    add jquery

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use("/jquery", express.static(__dirname + "/node_modules/jquery/dist/"));
 
 // メソッドオーバーライドの設定
 var methodOverride = require('method-override');
